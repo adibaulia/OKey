19aac46b1d4102dc177a5d6a5292b050075adee2
Unknown <adibaulia@gmail.com>
2017-11-09 11:43:55 +0700

    Maps Initial 1.0

--- app/src/main/java/com/example/android/okey/Splash.java
+++ app/src/main/java/com/example/android/okey/Splash.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.Window;
 
 public class Splash extends Activity {
 
@@ -15,16 +17,17 @@ public class Splash extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_splash);
-
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
+
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Intent mainIntent = new Intent(Splash.this,MapsMain.class);
                 Splash.this.startActivity(mainIntent);
                 Splash.this.finish();
+                findViewById(R.id.loadingPanel).setVisibility(View.GONE);
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
